c7319fdb36784939bb129eb296980546a9094f6b
Bogdan Oprea <bogdan.oprea@everymatrix.com>
2024-06-05 20:28:17 +0300

    feature(added functional scroll arrows to carousel)

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -3,36 +3,60 @@
 import Card from "./card";
 import { ChevronLeftIcon } from "@heroicons/react/16/solid";
 import { ChevronRightIcon } from "@heroicons/react/16/solid";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Carousel = (moviesList: any) => {
   const [maxScrollLeft, setMaxScrollLeft] = useState<Boolean>(true);
   const [maxScrollRight, setMaxScrollRight] = useState<Boolean>(false);
+  const ref = useRef<any>(null);
 
   const moviesListArr = moviesList.moviesList;
 
   const handleScroll = (e: any) => {
     const maxLeft = e.target.scrollLeft < 50;
-
     const maxRight =
       e.target.scrollLeft === e.target.scrollWidth - e.target.clientWidth;
 
-    if (maxLeft) setMaxScrollLeft(true);
-    if (maxRight) setMaxScrollRight(true);
-    if (!maxLeft) setMaxScrollLeft(false);
-    if (!maxRight) setMaxScrollRight(false);
+    if (maxLeft) {
+      setMaxScrollLeft(true);
+    } else {
+      setMaxScrollLeft(false);
+    }
+    if (maxRight) {
+      setMaxScrollRight(true);
+    } else {
+      setMaxScrollRight(false);
+    }
+  };
+
+  const scroll = (direction: "left" | "right") => {
+    requestAnimationFrame(() => {
+      const scrollLeft = ref.current.scrollLeft;
+      const itemWidth = parseInt(
+        getComputedStyle(ref.current.children[0]).width
+      );
+      if (direction === "left") {
+        ref.current.scrollLeft = scrollLeft - itemWidth * 3;
+      } else {
+        ref.current.scrollLeft = scrollLeft + itemWidth * 3;
+      }
+    });
   };
 
   return (
     <div className="relative">
       {!maxScrollLeft && (
-        <div className="hidden sm:block absolute left-0 top-40 z-30 justify-between">
-          <ChevronLeftIcon className="w-14 text-green-500 hover:cursor-pointer" />
+        <div
+          className="hidden sm:block absolute left-0 top-40 z-30 justify-between"
+          onClick={() => scroll("left")}
+        >
+          <ChevronLeftIcon className="w-14 text-green-500 hover:cursor-pointer bg-gradient-radial  rounded-full" />
         </div>
       )}
       <div
         className="relative flex gap-2 overflow-x-scroll sm:scrollbar sm:hover:scrollbar-thumb-green-500 sm:scrollbar-thumb-gray-600 sm:scrollbar-thumb-rounded-full sm:scrollbar-track-rounded-full"
         onScroll={handleScroll}
+        ref={ref}
       >
         {moviesListArr.map((movie: any, index: number) => (
           <Card
@@ -43,8 +67,11 @@ const Carousel = (moviesList: any) => {
         ))}
       </div>
       {!maxScrollRight && (
-        <div className="hidden sm:block absolute right-0 top-40 z-30 justify-between">
-          <ChevronRightIcon className="w-14 text-green-500 hover:cursor-pointer" />
+        <div
+          className="hidden sm:block absolute right-0 top-40 z-30 justify-between"
+          onClick={() => scroll("right")}
+        >
+          <ChevronRightIcon className="w-14 text-green-500 hover:cursor-pointer bg-gradient-radial rounded-full" />
         </div>
       )}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,8 @@ const config: Config = {
         "150full": "150vw",
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial":
+          "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 70%)",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
